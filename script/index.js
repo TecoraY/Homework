dc8a20c8bb82d4675f34ef8daec9b87981b7483d
TecoraY <102709566+TecoraY@users.noreply.github.com>
2022-04-08 03:25:34 -0400

    homework with js T. Yisrael

---
+++ script/index.js
@@ -0,0 +1,3 @@
+let loadedTacos=[ "Vegan Tacos- Walnut Meat", "Guacamole", "Pico De Gallo", "Corn Salsa"]
+console.log(loadedTacos)
+//How to incorporate javascript? what kind of elements should my html have. and how do i make both connnect and make sense??
